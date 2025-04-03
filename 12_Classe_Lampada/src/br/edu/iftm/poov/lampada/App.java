7347ce5c914ea1b4d309ae3dd67c0383486cef50
Joloupa <jaopsds@gmail.com>
2025-04-03 18:48:28 -0300

    Meu commit

--- 12_Classe_Lampada/src/br/edu/iftm/poov/lampada/App.java
+++ 12_Classe_Lampada/src/br/edu/iftm/poov/lampada/App.java
@@ -4,9 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
        // System.out.println("Hello, World!");
 
-       Lampada l1 = new Lampada();
-       Lampada l2 = new Lampada(110, 100);
-       Lampada l3 = new Lampada(-1000, -1000, true);
+    //    Lampada l1 = new Lampada();
+    //    Lampada l2 = new Lampada(110, 100);
+    //    Lampada l3 = new Lampada(-1000, -1000, true);
 
 
        LampaadaColorida lc1 = new LampaadaColorida();
@@ -14,17 +14,23 @@ public class App {
        lc1.acender();
        lc1.mostrarNaTela();
 
-       //l2.setAcesa(true);
-       l2.acender();
-       //l2.setAcesa(false);
-       l2.apagar();
+       LampaadaColorida lc2 = new LampaadaColorida(220, 100, "Vermelha");
+       lc2.mostrarNaTela();
+
+       LampaadaColorida lc3 = new LampaadaColorida(220, 100, "Amarela");
+       lc3.mostrarNaTela();
+
+    //    l2.setAcesa(true);
+    //    l2.acender();
+    //    //l2.setAcesa(false);
+    //    l2.apagar();
 
 
-       l3.setTensao(-1000);
-       l3.setPotencia(-1000);
-       l3.acender();
-       l3.queimar();
-       l3.quebrar();
+    //    l3.setTensao(-1000);
+    //    l3.setPotencia(-1000);
+    //    l3.acender();
+    //    l3.queimar();
+    //    l3.quebrar();
 
 
          // l2.setTensao(220);
@@ -32,8 +38,8 @@ public class App {
         // if (l3.isAcesa()) {      
         // }
 
-        l1.mostrarNaTela();
-        l2.mostrarNaTela();
-        l3.mostrarNaTela();
+        // l1.mostrarNaTela();
+        // l2.mostrarNaTela();
+        // l3.mostrarNaTela();
     }
 }
--- 12_Classe_Lampada/src/br/edu/iftm/poov/lampada/LampaadaColorida.java
+++ 12_Classe_Lampada/src/br/edu/iftm/poov/lampada/LampaadaColorida.java
@@ -5,7 +5,14 @@ public class LampaadaColorida extends Lampada {
     private String cor;
 
     public LampaadaColorida(){
-        
+        super(100,60,false);
+        System.out.println("Construtor padrao LampadaColorida executado");
+        cor = "Azul";
+    }
+
+    public LampaadaColorida(int tensao, int potencia, String cor){
+        super(tensao, potencia);
+        this.cor = cor;
     }
 
     public String getCor() {
@@ -16,5 +23,9 @@ public class LampaadaColorida extends Lampada {
         this.cor = cor;
     }
 
-    
+    @Override
+    public void mostrarNaTela(){
+        super.mostrarNaTela();
+        System.out.println(cor);
+    }
 }
